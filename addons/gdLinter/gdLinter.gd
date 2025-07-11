723991fe700482dc303a9b5c0112e9d6cc5d4133
shap-po <50052124+shap-po@users.noreply.github.com>
2025-07-11 08:09:11 +0300

    feat: Run linting in a non-blocking process

--- addons/gdLinter/gdLinter.gd
+++ addons/gdLinter/gdLinter.gd
@@ -22,11 +22,13 @@ var highlight_lines: PackedInt32Array
 var item_lists: Array[ItemList]
 var script_editor: ScriptEditor
 
+var proc: Dictionary
+var current_path: String
+
 var _dock_ui: GDLinterDock
 var _is_gdlint_installed: bool
 var _ignore: Resource
 var _gdlint_path: String
-
 
 func _enter_tree() -> void:
 	# add default settings values
@@ -47,12 +49,26 @@ func _enter_tree() -> void:
 	get_gdlint_version()
 	prints("Loading GDLint Plugin success")
 
+
 func _process(_delta: float) -> void:
+	# Check if the linter sub-process finished
+	if proc:
+		var exit_code: int = OS.get_process_exit_code(proc["pid"])
+		if exit_code != -1:
+			# Read the output and process it
+			var std: FileAccess = proc["stdio"] if exit_code == 0 else proc["stderr"]
+			var outout: String = std.get_as_text(true)
+
+			process_lint(outout)
+			# Mark as complete
+			proc = {}
+
 	if not get_current_editor():
 		return
 
 	if not highlight_lines.is_empty():
 		set_line_color(warning_color)
+
 
 func exec(path: String, arguments: PackedStringArray, output: Array = [],
 	read_stderr: bool = false, open_console: bool = false):
@@ -61,6 +77,19 @@ func exec(path: String, arguments: PackedStringArray, output: Array = [],
 		OS.execute("CMD.exe", args, output, read_stderr, open_console)
 	else:
 		OS.execute(path, arguments, output, read_stderr, open_console)
+
+
+func exec_non_block(path: String, arguments: PackedStringArray) -> Dictionary:
+	var res: Dictionary
+
+	if OS.get_name() == "Windows":
+		var args = PackedStringArray(["/C"]) + PackedStringArray([path]) + arguments
+		res = OS.execute_with_pipe("CMD.exe", args, false)
+	else:
+		res = OS.execute_with_pipe(path, arguments, false)
+
+	return res
+
 
 func _on_editor_script_changed(script: Script) -> void:
 	_dock_ui.clear_items()
@@ -96,17 +125,22 @@ func on_resource_saved(resource: Resource) -> void:
 	# Show resource path in the GDLint Dock
 	_dock_ui.file.text = resource.resource_path
 
-	# Execute linting and get its output
-	var filepath: String = ProjectSettings.globalize_path(resource.resource_path)
-	var gdlint_output: Array = []
-	var output_array: PackedStringArray
-	var exit_code = exec(_gdlint_path, [filepath], gdlint_output, true)
-	if not exit_code == -1:
-		var output_string: String = gdlint_output[0]
-		output_array = output_string.replace(filepath+":", "Line ").split("\n")
+	run_lint(resource)
 
-		_dock_ui.set_problems_label(_dock_ui.num_problems)
-		_dock_ui.set_ignored_problems_label(_dock_ui.num_ignored_problems)
+
+func run_lint(resource: GDScript) -> void:
+	# Execute linting and get output
+	var filepath: String = ProjectSettings.globalize_path(resource.resource_path)
+
+	proc = exec_non_block(_gdlint_path, [filepath])
+	current_path = filepath
+
+
+func process_lint(output: String) -> void:
+	var output_array: PackedStringArray = output.replace(current_path + ":", "Line ").split("\n")
+
+	_dock_ui.set_problems_label(_dock_ui.num_problems)
+	_dock_ui.set_ignored_problems_label(_dock_ui.num_ignored_problems)
 
 	# Workaround until unique name bug is fixed
 	# https://github.com/Scony/godot-gdscript-toolkit/issues/284
@@ -122,15 +156,15 @@ func on_resource_saved(resource: Resource) -> void:
 		return
 
 	# When errors are found create buttons in the dock
-	for i in output_array.size()-2:
+	for i in output_array.size() - 2:
 		var regex := RegEx.new()
 		regex.compile("\\d+")
 		var result := regex.search(output_array[i])
 		if result:
-			var current_line := int(result.strings[0])-1
+			var current_line := int(result.strings[0]) - 1
 			var error := output_array[i].rsplit(":", true, 1)
 			if len(error) > 1:
-				_dock_ui.create_item(current_line+1, error[1])
+				_dock_ui.create_item(current_line + 1, error[1])
 				if _dock_ui.is_error_ignored(error[1]):
 					continue
 				highlight_lines.append(current_line)
@@ -159,14 +193,13 @@ func set_line_color(color: Color) -> void:
 
 	for line: int in highlight_lines:
 		# Skip line if this one is from the old code editor
-		if line > current_code_editor.get_line_count()-1:
+		if line > current_code_editor.get_line_count() - 1:
 			continue
 		# Skip if something else is highlighted
 		if current_code_editor.get_line_background_color(line) != NO_COLOR and color != NO_COLOR:
 			continue
 
-		current_code_editor.set_line_background_color(line,
-			color.darkened(0.5))
+		current_code_editor.set_line_background_color(line, color.darkened(0.5))
 
 
 func clear_highlights() -> void:
